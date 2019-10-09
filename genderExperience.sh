1dd235fbf4bd98d8ab26119fbef13af2f4150540
Carly Barbera <cbarbera@nd.edu>
2019-10-09 14:01:55 -0400

    Created script to execute Ex5 task 1

--- genderExperience.sh
+++ genderExperience.sh
@@ -1,2 +1,5 @@
-# Exercise5 shell script
-
+# Exercise5 
+# script to sort wages.csv gender and years experience 
+# first by gender, then by years experience 
+# usage: bash genderExperience.sh
+cat wages.csv | cut -d , -f 1,2 | tr "," " " | sort -k1,1 -k2,2n | sed '/gender/d' > task1.sh
