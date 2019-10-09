588921a1b90ff5976845f5e3a73fb2fcb679494a
Carly Barbera <cbarbera@nd.edu>
2019-10-09 15:38:44 -0400

    Last edit

--- genderExperience.sh
+++ genderExperience.sh
@@ -1,8 +1,8 @@
 # Exercise5
 # Task 1: script to sort wages.csv gender and years experience
 # first by gender, then by years experience
-# usage: bash genderExperience.sh
-cat wages.csv | cut -d , -f 1,2 | tr "," " " | sort -k1,1 -k2,2n | sed '/gender/d' > task1.sh
+# usage: bash genderExperience.sh wages.csv
+cat $@ | cut -d , -f 1,2 | tr "," " " | sort -k1,1 -k2,2n | sed '/gender/d' > task1.sh
 
 
 # Task 2: prints the information about the highest and lowest earners and number of women in top ten
