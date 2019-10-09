1ce0f03317b0e79c57d6a2b2251fb239e93d623e
Carly Barbera <cbarbera@nd.edu>
2019-10-09 14:21:23 -0400

    Task1,2

--- genderExperience.sh
+++ genderExperience.sh
@@ -8,6 +8,25 @@ cat wages.csv | cut -d , -f 1,2 | tr "," " " | sort -k1,1 -k2,2n | sed '/gender/
 # Exercise5 shell script
 # Task 1: creates a file containing unique combinations of gender and years of experience
 # Task 2: prints the information about the highest and lowest earners and number of women in top ten
+<<<<<<< HEAD
+=======
+
+
+
+
+
+
+
+
+
+echo "Highest earner: " 
+sort --field-separator=',' -n -r -k4 $@ | head -n 1
+echo "Lowest earner: " 
+sort --field-separator=',' -n -r -k4 $@ | tail -n 2 | head -n 1
+echo "# of women in top 10 earners: " 
+sort --field-separator=',' -n -r -k4 $@ | head -n 10 | grep female | wc -l
+
+>>>>>>> refs/remotes/origin/master
 
 
 
